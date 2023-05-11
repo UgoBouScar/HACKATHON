d1c8d0fcc2ee6db528778dced23c6721c4a7ac09
Youcef Aissat <y.aissat@gmail.com>
2023-05-11 22:35:54 +0200

    add footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,35 @@
+import React, { useState } from "react";
+import "../sass/Footer.scss";
+
 export default function Footer() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(email, message);
+  };
+
   return (
-    <footer id="footer">
-      <h2>text</h2>
-    </footer>
+    <div className="footer">
+      <h1>Contactez nous</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
   );
 }
