580aa0df0498f33feae55ffbfc6565135716967f
nefzina <nefzi.amani@gmail.com>
2023-05-11 23:59:35 +0200

    added map from leaflet but cannot display it entirely

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,9 +4,11 @@ import NavBar from "@components/NavBar";
 import Footer from "@components/Footer";
 import icon from "@assets/icons-marqueur.png";
 import "../sass/home.scss";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function Home() {
   const [modalOpen, setModalOpen] = useState(false);
+  const position = [51.505, 50.09];
 
   return (
     <div>
@@ -14,6 +16,17 @@ export default function Home() {
       <div className="home">
         <div className="background-container" />{" "}
         <div className="map-container">
+          <MapContainer center={position} zoom={15} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
           <button
             type="button"
             className="openModalBtn"
