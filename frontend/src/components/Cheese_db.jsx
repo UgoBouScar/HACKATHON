901607371071ab5340ed65c578d02f1b3f8e6400
Alaric HENROT <alaric.henrot@gmail.com>
2023-05-12 10:39:10 +0200

    add cheese lat,long

--- frontend/src/components/Cheese_db.jsx
+++ frontend/src/components/Cheese_db.jsx
@@ -4,6 +4,7 @@
   {
     id: 1,
     name: "Roquefort",
+    position_marker: [44.32579, 3.05917],
     image:
       "https://www.regal.fr/sites/art-de-vivre/files/roquefort-fromage-bleu_istock.jpg",
     region: "Aveyron",
@@ -13,6 +14,7 @@
   {
     id: 2,
     name: "Comté",
+    position_marker: [47.234329, 4.80681],
     image:
       "https://th.bing.com/th/id/OIP.ftua5s-CspAVdxyNSbrUTAHaD9?pid=ImgDet&rs=1",
     region: "Franche-Comté",
@@ -22,6 +24,7 @@
   {
     id: 3,
     name: "Brie",
+    position_marker: [48.674378, 2.691357],
     image:
       "https://all-andorra.com/wp-content/uploads/2018/12/Tout-ce-quil-faut-savoir-sur-le-l%C3%A9gendaire-fromage-fran%C3%A7ais-le-Brie.jpg",
     region: "Île-de-France",
@@ -31,6 +34,7 @@
   {
     id: 4,
     name: "Camembert",
+    position_marker: [49.259599, 0.180678],
     image:
       "https://www.cellartours.com/wp-content/uploads/2018/12/camembert-cheese.jpg",
     region: "Normandie",
@@ -40,6 +44,7 @@
   {
     id: 5,
     name: "Munster",
+    position_marker: [48.622045, 7.508458],
     image:
       "https://th.bing.com/th/id/OIP.fvB9xW2DTL84mI3hTaidLwHaE8?pid=ImgDet&rs=1",
     region: "Alsace-Lorraine",
@@ -49,6 +54,7 @@
   {
     id: 6,
     name: "Saint-Nectaire",
+    position_marker: [45.589478, 2.994237],
     image:
       "https://th.bing.com/th/id/R.759275e5fde71e5448a575c2a1cfde3b?rik=fpxWGr2TflJK8w&riu=http%3a%2f%2ffromageriedufour.com%2fWebRoot%2fStore9%2fShops%2fd6f1586e-3c4e-43ef-80d0-782d13021ef5%2f56DD%2f73BD%2f8D2C%2fF72A%2f927A%2f0A48%2f351F%2f2F8A%2fst_nectaire.JPG&ehk=WPjhqmTo0VVwVy%2bf7rKSRU7ridBwXiM82hevDrLnMB0%3d&risl=&pid=ImgRaw&r=0",
     region: "Auvergne-Rhône-Alpes",
@@ -58,6 +64,7 @@
   {
     id: 7,
     name: "Reblochon",
+    position_marker: [46.181762, 6.824638],
     image:
       "https://th.bing.com/th/id/OIP.mDE1I_k_jRN3hTg-YYOTfgHaE6?pid=ImgDet&rs=1",
     region: "Savoie",
@@ -67,6 +74,7 @@
   {
     id: 8,
     name: "Tomme de Savoie",
+    position_marker: [45.786739, 6.535353],
     image:
       "https://i.pinimg.com/originals/43/54/13/4354138e0a12e5890a367945bf6f7b8e.jpg",
     region: "Savoie",
@@ -76,6 +84,7 @@
   {
     id: 9,
     name: "Mimolette",
+    position_marker: [50.757364, 2.613226],
     image:
       "https://resize-elle.ladmedia.fr/rcrop/638,,forcex/img/var/plain_site/storage/images/elle-a-table/les-dossiers-de-la-redaction/dossier-de-la-redac/voici-les-40-fromages-preferes-des-francais/mimolette/90390261-1-fre-FR/Mimolette.jpg",
     region: "Nord-Pas-de-Calais ",
@@ -85,6 +94,7 @@
   {
     id: 10,
     name: "Pont-l’Évêque",
+    position_marker: [49.669128, 1.168256],
     image:
       "https://grainedeschamps.com/boutique/379-medium_default/pont-l-eveque-aop-fermier-au-lait-cru.jpg",
     region: "Normandie",
@@ -94,6 +104,7 @@
   {
     id: 11,
     name: "Fourme d’Ambert",
+    position_marker: [45.548059, 3.738959],
     image:
       "https://th.bing.com/th/id/OIP.KfvZDqkkWnxP7N9QxVjiPwHaEr?pid=ImgDet&w=886&h=559&rs=1",
     region: "Auvergne-Rhône-Alpes",
@@ -102,7 +113,8 @@
   },
   {
     id: 12,
-    name: "Chèvre",
+    name: "bûche de Chèvre",
+    position_marker: [46.068824, -0.586657],
     image:
       "https://cache.marieclaire.fr/data/photo/w1000_ci/54/fromage-de-chevre.jpg",
     region: "Poitou-Charentes",
@@ -112,6 +124,7 @@
   {
     id: 13,
     name: "Coulommiers",
+    position_marker: [48.805818, 3.091796],
     image: "",
     region: "Île-de-France",
     description:
@@ -120,6 +133,7 @@
   {
     id: 14,
     name: "Maroilles",
+    position_marker: [50.230389, 3.675155],
     image:
       "https://th.bing.com/th/id/OIP.PPyjOvb1lM40of8VkGBlVwHaE8?pid=ImgDet&rs=1",
     region: "Nord-Pas-de-Calais",
@@ -129,6 +143,7 @@
   {
     id: 15,
     name: "Cantal",
+    position_marker: [45.283829, 2.724159],
     image:
       "https://th.bing.com/th/id/OIP.4tU8PR3EjFxPuoIPve2TLgHaE8?pid=ImgDet&rs=1",
     region: "Auvergne-Rhône-Alpes",
