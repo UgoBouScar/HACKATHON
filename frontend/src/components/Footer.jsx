bd14263493a475aed7c505d08e390fec67b2c116
Youcef Aissat <y.aissat@gmail.com>
2023-05-12 11:24:15 +0200

    update_footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,31 +4,42 @@ import "../sass/Footer.scss";
 export default function Footer() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // eslint-disable-next-line no-restricted-syntax
-    console.log(email, message);
+    console.log(name, email, message);
   };
 
   return (
     <div className="footer">
       <img
         className="footer--img"
-        src="../src/assets/image_footer.jpg"
+        src="../src/assets/footerbg_img.jpg"
         alt="imagefooter"
       />
       <div className="contact-info">
-        <h2>Contactez nous</h2>
+        <h2 className="footer--title">Contactez nous</h2>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="contact-email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <div className="name--email">
+            <label htmlFor="text">Nom</label>
+            <input
+              type="text"
+              id="text"
+              className="contact-name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="contact-email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
           <label htmlFor="message">Message</label>
           <textarea
             id="message"
